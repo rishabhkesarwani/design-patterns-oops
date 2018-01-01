76d855ec2e60baa2c94f6debc6f7c7d596f32dfc
Rishabh Kesarwani <rishabh@mydidi.in>
2018-01-01 12:32:57 +0530

    Singleton Design - Good Code

--- src/com/rishabhkesarwani/StoreListing.java
+++ src/com/rishabhkesarwani/StoreListing.java
@@ -4,11 +4,20 @@ import java.util.ArrayList;
 
 public class StoreListing {
 
+    private static StoreListing onlyInstance = null;
+
     private ArrayList<Store> storeList = new ArrayList<>();
 
     private StoreListing() {
         this.listStore(new TanishaStore());
         this.listStore(new SikingStore());
+    }
+
+    public static StoreListing getInstance() {
+        if (onlyInstance == null) {
+            onlyInstance = new StoreListing();
+        }
+        return onlyInstance;
     }
 
     public ArrayList<Store> getStoreList() {
